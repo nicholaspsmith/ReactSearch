23f9b4dcbe851fe20938503af7450d4002af74c9
Nick P Smith <npsmith1990@gmail.com>
2016-06-11 22:10:51 -0600

    use searchbar to filter list of ingredients

--- src/index.js
+++ src/index.js
@@ -1,14 +1,60 @@
 import React, { Component } from 'react'
 import ReactDOM from 'react-dom'
 
+const ingredients = [
+  {
+    name: 'hops',
+    id: 1
+  },
+  {
+    name: 'grain',
+    id: 2
+  },
+  {
+    name: 'barley',
+    id: 3
+  }
+]
+
 class App extends Component {
+  constructor(props) {
+    super(props);
+  
+    this.state = {
+      term: ''
+    };
+
+    this.onInputChange = this.onInputChange.bind(this)
+  }
+
+  onInputChange(e) {
+    this.setState({
+      term: e.target.value
+    })
+  }
+
   render() {
     return (
-      <div className="container">
-        To get started, edit /src/index.js
+      <div>
+        <input 
+          type="text" 
+          onChange={this.onInputChange} 
+          value={this.state.term}
+          placeholder="Search" />
+        <div>Ingredients:</div>
+        <ul>
+          {
+            ingredients.filter( ingredient => {
+              return ingredient.name.indexOf(this.state.term) > -1
+            }).map( ingredient => {
+              return <div>{ingredient.name}</div>
+            })
+          }
+        </ul>
       </div>
     )
   }
+
 }
 
-ReactDOM.render(<App />, document.getElementById('ReactApp'))
+ReactDOM.render(<App ingredients={ingredients} />, document.getElementById('app'));
